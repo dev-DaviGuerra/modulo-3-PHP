8c37c723ba4d27ae596fc639dd43f9d4176fe39c
DvGuerraa <davimguerra@gmail.com>
2025-01-28 13:51:12 -0300

    FormularioSimples

--- dados.php
+++ dados.php
@@ -1,15 +1,8 @@
 <?php
 
-//Superglobais no PHP
-//São variaveis que estão definidas para a gente no escopo global
-// $_GET
-//isset()
-//empty
+if (!empty($_GET['login'] && !empty($_GET['password']))) {
+    $login = $_GET['login'];
+    $password = $_GET['password'];
 
-
-if (!empty($_GET['campanha'])) {
-    $numero_campanha = $_GET['campanha'];
-    echo 'Você veio pela campanha'.$numero_campanha;
-} else {
-    echo 'Variável não definida ou vazia';
+    echo "Seu nome é $login e sua senha é $password";
 }
