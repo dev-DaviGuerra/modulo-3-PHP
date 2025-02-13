51b385120cb15368d1d632fc1903a14fad0cda49
DvGuerraa <davimguerra@gmail.com>
2025-02-13 14:31:18 -0300

    CoalescênciaNula

--- dados.php
+++ dados.php
@@ -23,13 +23,14 @@ if (!empty($_POST['login']) && !empty($_POST['password'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulário de login</title>
 </head>
 <body>
-    <form action="dados.php" method = "POST">
-        <input type="text" name="login" placeholder="Digite o login"/>
+    <form action="" method = "POST">
+        <input type="text" name="login" placeholder="Digite o login" value= "<?= $login ?? '' ?>"/>
         <br />
-        <input type="password" name="password" placeholder="Digite a senha">
+        <input type="password" name="password" placeholder="Digite a senha" value="<?= $password ?? '' ?>">
+
         <input type="submit" value="Enviar">
         <?php if (!empty($mensagem)){
         echo $mensagem;}?>
